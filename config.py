68e18e8574f31d134539625c8c15346463f7c894
asilbek <asilbekmirolimov@gmail.com>
2025-05-21 09:12:44 +0500

    config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BOT_TOKEN='7770700132:AAHjOyk5_kAmUApgbJuK1oFhFoXrDGaxITQ'
+BOT_TOKEN='7770700132:AAEPvi5Db7fQ8xbGyd3xWImUr1mu2o2rk1Y'
 CHANNELS_URL = ['https://t.me/asilbekmirolimov', 
                 'https://t.me/Abduqahhor_is_history',
                 'https://t.me/trentmyuzik']
